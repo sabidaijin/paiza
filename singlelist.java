ce39453d5ca0c1213158938bc679e4d85c436182
sabidaijin <yamahb@icloud.com>
2024-01-25 22:44:31 +0900

    単方向リスト

---
+++ singlelist.java
@@ -0,0 +1,47 @@
+public class singlelist {
+    public static void main(String[] args){
+        list list1=new list();
+        Node node1=new Node();
+        node1.data=1;
+        list1.append(node1);
+        list1.show();   
+
+
+    }
+    
+}
+
+class Node{
+    int data;
+    Node next;
+
+}
+
+class list{
+    Node head;
+    void show(){
+        System.out.println(this.head.data);
+    }
+    void append(Node node){
+        if(this.head==null){
+            this.head=node;
+            return;
+        }else{
+            Node current=this.head;
+            while(current.next!=null){
+                current=current.next;
+            }
+            current.next=node;
+        
+        }
+         
+    }
+    void delete(){
+        Node current=this.head;
+            while(current.next!=null){
+                current=current.next;
+            }
+            current.next=null;
+        
+    }
+}
