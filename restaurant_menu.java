20e4e53117041bb7bcecadd7dff3e9921ee85cd8
sabidaijin <yamahb@icloud.com>
2024-01-27 23:42:01 +0900

    00127

--- restaurant_menu.java
+++ restaurant_menu.java
@@ -1,21 +1,33 @@
-
-// あなたは、ある料理店でメニューを見ています。この料理店の料理名は複雑で、ある料理が食べたい料理であるか瞬時に判断するのが難しいです。
-
-// あなたが食べたい料理の名前と、メニューに載っている料理名が 1 つ与えられるので、食べたい料理に合致するメニューであれば "Yes" を、そうでなければ "No" を出力するプログラムを作成してください。
-// 食べたい料理に合致するメニューとは、メニューの単語の中に食べたい料理名が入っていることを指します。
-
-// 入力例 1 では、あなたは hamburg を食べたいと思っていて、与えられた料理名は cheese hamburg です。2 つ目の単語が、あなたの食べたい hamburg と一致しているため、"Yes" と出力します。
-
 import java.util.Scanner;
 
-public class restaurant_menu{
+public class restaurant_menu {
 
-    public static void main (String [] args){
+    public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        
-        String line=sc.nextLine();
-        
 
+        String line = sc.nextLine();
+        int count = sc.nextInt();
+        sc.nextLine(); // 改行を読み飛ばす
+
+        String[] menu = new String[count];
+        menu = sc.nextLine().split(" ");
+
+        sc.close();
+
+        boolean found = false; // 一致する料理が見つかったかどうかを示すフラグ
+
+        for (int k = 0; k < menu.length; k++) {
+            if (menu[k].equals(line)) {
+                found = true;
+                break; // 一致する料理が見つかったらループを抜ける
+            }
+        }
+
+        if (found) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
     }
 }
---
+++ simplesum.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class simplesum {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        Integer line = sc.nextInt();
+        Integer line2 = sc.nextInt();
+        sc.close();
+        
+        Integer sum=line + line2;
+        
+        
+        System.out.println(sum);
+    }
+}
