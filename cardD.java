8de6e93f9c5ed14aada3027769f1f1dfac2ebc83
sabidaijin <yamahb@icloud.com>
2024-02-01 20:06:12 +0900

    0201

---
+++ cardD.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class cardD {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        Integer a = sc.nextInt();
+         Integer b= sc.nextInt();
+        System.out.println(a-b);
+        
+        
+    }
+}
